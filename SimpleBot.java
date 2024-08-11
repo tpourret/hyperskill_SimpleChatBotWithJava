f4ca0c72f57f556d9752314a1a1e3a9088c2094b
Théo POURRET <72062720+tpourret@users.noreply.github.com>
2024-08-11 13:54:25 +1100

    SimpleBot.java - Stage 2

--- SimpleBot.java
+++ SimpleBot.java
@@ -1,10 +1,17 @@
 package bot;
 
+import java.util.Scanner;
+
 public class SimpleBot {
     public static void main(String[] args) {
-        String botName = "Aid";
-        int birthYear = 2023;
-        System.out.printf("Hello! My name is %s\n" +
-                "I was created in %d", botName, birthYear);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hello! My name is Aid.");
+        System.out.println("I was created in 2018.");
+        System.out.println("Please, remind me your name.");
+
+        String name = scanner.nextLine();
+
+        System.out.println("What a great name you have, " + name + "!");
     }
 }
