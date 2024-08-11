f678a8c1a9fd70a58ee52bb08777cf135a3096d9
Théo POURRET <72062720+tpourret@users.noreply.github.com>
2024-08-11 11:35:29 +1100

    Stage 1 - Create SimpleBot.java
    Objective
    For the first stage, you will write a bot that displays a greeting, its name, and the year it was created. First impressions count!
    
    Your program should print the following lines:
    
    Hello! My name is {botName}.
    I was created in {birthYear}.
    
    Instead of {botName}, print any name you choose and replace {birthYear} with the current year (four digits). You don't need to take any input at this stage.
    
    Example
    Output:
    
    Hello! My name is Aid.
    I was created in 2023.
    
    Here's the result with your bot named Aid and 2023 as its birth year. Feel free to modify the values, but make sure to print exactly two lines.

---
+++ SimpleBot.java
@@ -0,0 +1,10 @@
+package bot;
+
+public class SimpleBot {
+    public static void main(String[] args) {
+        String botName = "Aid";
+        int birthYear = 2023;
+        System.out.printf("Hello! My name is %s\n" +
+                "I was created in %d", botName, birthYear);
+    }
+}
