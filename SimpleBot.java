29644b23f5046dff35b759278366550831a8cb1c
Théo POURRET <72062720+tpourret@users.noreply.github.com>
2024-08-11 14:02:15 +1100

    Update SimpleBot.java - Stage 3

--- SimpleBot.java
+++ SimpleBot.java
@@ -13,5 +13,15 @@ public class SimpleBot {
         String name = scanner.nextLine();
 
         System.out.println("What a great name you have, " + name + "!");
+        System.out.println("Let me guess your age.");
+        System.out.println("Enter remainders of dividing your age by 3, 5 and 7.");
+
+        // reading all remainders
+        int remainder3 = scanner.nextInt();
+        int remainder5 = scanner.nextInt();
+        int remainder7 = scanner.nextInt();
+        int age = (remainder3 * 70 + remainder5 * 21 + remainder7 * 15) % 105;
+
+        System.out.println("Your age is " + age +"; that's a good time to start programming!");
     }
 }
